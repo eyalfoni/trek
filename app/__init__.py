a490dfa9c4214f88440b0a22995e34fcddd7028a
Eyal Foni <eyalfoni@gmail.com>
2020-07-13 18:05:01 -0400

    remove old code

--- app/__init__.py
+++ app/__init__.py
@@ -42,23 +42,7 @@ def create_app(config_class=Config):
     # logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
     if not app.debug and not app.testing:
-        if app.config['MAIL_SERVER']:
-            auth = None
-            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-                auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-            secure = None
-            if app.config['MAIL_USE_TLS']:
-                secure = ()
-            mail_handler = SMTPHandler(
-                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-                toaddrs=app.config['ADMINS'], subject='Trek Failure',
-                credentials=auth, secure=secure
-            )
-            mail_handler.setLevel(logging.ERROR)
-            app.logger.addHandler(mail_handler)
-
-        if app.config['c']:
+        if app.config['LOG_TO_STDOUT']:
             stream_handler = logging.StreamHandler()
             stream_handler.setLevel(logging.INFO)
             app.logger.addHandler(stream_handler)
